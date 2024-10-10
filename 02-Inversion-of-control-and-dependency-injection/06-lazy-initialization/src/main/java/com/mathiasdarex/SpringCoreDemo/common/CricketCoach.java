a315367dd9453d9e8909f2386aa935e23f3bb59f
MathiasDaRex <87494005+MathiasDaRex@users.noreply.github.com>
2024-10-10 20:23:43 +0200

    Add files via upload

---
+++ 02-Inversion-of-control-and-dependency-injection/06-lazy-initialization/src/main/java/com/mathiasdarex/SpringCoreDemo/common/CricketCoach.java
@@ -0,0 +1,16 @@
+package com.mathiasdarex.SpringCoreDemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+
+    public CricketCoach() {
+        System.out.println("In construction: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes :)";
+    }
+}
