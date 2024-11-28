246a6b8e1345121d9d368be70ea0f66675cf7937
MathiasDaRex <87494005+MathiasDaRex@users.noreply.github.com>
2024-11-28 17:54:46 +0100

    Add files via upload

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/mathiasdarex/springboot/crudDemo/entity/Employee.java
@@ -0,0 +1,84 @@
+package com.mathiasdarex.springboot.crudDemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+
+    // define field
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+
+    // we have to name the column the same as in the database
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    // define constructors
+
+    public Employee(int id) {
+    }
+
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // define getter/setter
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    // define toString
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
