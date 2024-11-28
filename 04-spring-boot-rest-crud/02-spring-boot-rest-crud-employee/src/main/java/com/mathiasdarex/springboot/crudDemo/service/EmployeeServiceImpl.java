b1f3c50aecf0f951805ee44b09644e3c1bdbe3d5
MathiasDaRex <87494005+MathiasDaRex@users.noreply.github.com>
2024-11-28 19:52:37 +0100

    Add files via upload
    made use of the service layer, delegated tasks to the DAO, @Transactional at the service methods that make an update to the database.

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/mathiasdarex/springboot/crudDemo/service/EmployeeServiceImpl.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/mathiasdarex/springboot/crudDemo/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.mathiasdarex.springboot.crudDemo.dao.EmployeeDAO;
 import com.mathiasdarex.springboot.crudDemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -24,5 +25,23 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    // we update the database so we have to use the @Transactional annotation right here at the service layer
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 }
