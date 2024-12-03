21f3659a66833a069cfbda036d540f4d58cc9ea1
MathiasDaRex <87494005+MathiasDaRex@users.noreply.github.com>
2024-12-03 20:53:04 +0100

    Add files via upload
    Spring Data REST configs

--- 04-spring-boot-rest-crud/04-spring-boot-crud-employee-with-spring-data-rest/src/main/java/com/mathiasdarex/springboot/crudDemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-crud-employee-with-spring-data-rest/src/main/java/com/mathiasdarex/springboot/crudDemo/dao/EmployeeRepository.java
@@ -2,9 +2,13 @@ package com.mathiasdarex.springboot.crudDemo.dao;
 
 import com.mathiasdarex.springboot.crudDemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // to get the CRUD methods out of the box, we just need to extend the JpaRepository interface,
 // and type in our entity type, and primary key type
+
+// to expose the methods on another endpoint, we can use @RepositoryRestResource()
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it, we don't need to write any code for the CRUD operations
