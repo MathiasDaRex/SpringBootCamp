8af24176384eacffe3dbbeb5036e6f1f2c1a6a9b
MathiasDaRex <87494005+MathiasDaRex@users.noreply.github.com>
2025-01-07 19:34:33 +0100

    Add files via upload

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/mathiasdarex/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/mathiasdarex/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.mathiasdarex.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,4 +23,24 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+    // need a controller method to read form data
+    // and add data to the model
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create a message
+        String result = "Hi! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
 }
