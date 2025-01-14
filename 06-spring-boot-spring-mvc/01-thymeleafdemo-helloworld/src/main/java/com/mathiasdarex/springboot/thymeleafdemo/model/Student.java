e3bc5fc127493ce8ca40a9a2c81bb62a8eba9e2f
MathiasDaRex <87494005+MathiasDaRex@users.noreply.github.com>
2025-01-14 20:15:57 +0100

    Add files via upload
    hardcoded checkboxes

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/mathiasdarex/springboot/thymeleafdemo/model/Student.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/mathiasdarex/springboot/thymeleafdemo/model/Student.java
@@ -1,11 +1,14 @@
 package com.mathiasdarex.springboot.thymeleafdemo.model;
 
+import java.util.List;
+
 public class Student {
 
     private String firstName;
     private String lastName;
     private String country;
     private String favoriteLanguage;
+    private List<String> favoriteSystems;
 
     public Student() {
     }
@@ -31,6 +34,11 @@ public class Student {
         return favoriteLanguage;
     }
 
+    public List<String> getFavoriteSystems() {
+        return favoriteSystems;
+    }
+
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
@@ -41,5 +49,9 @@ public class Student {
 
     public void setFavoriteLanguage(String favoriteLanguage) {
         this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public void setFavoriteSystems(List<String> favoriteSystems) {
+        this.favoriteSystems = favoriteSystems;
     }
 }
