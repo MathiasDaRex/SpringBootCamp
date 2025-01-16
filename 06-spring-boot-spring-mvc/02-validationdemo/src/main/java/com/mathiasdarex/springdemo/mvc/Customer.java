d041b889e6b23df434a9704717e1daaa5b65bf51
MathiasDaRex <87494005+MathiasDaRex@users.noreply.github.com>
2025-01-16 19:57:26 +0100

    Add files via upload
    - added regex validation to postal code
    - made integer field required 
    - made messages.properties file to make custom error messages !!! file location and name very important !!! 
    - added custom error message to String-Integer type mismatch,

--- 06-spring-boot-spring-mvc/02-validationdemo/src/main/java/com/mathiasdarex/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/02-validationdemo/src/main/java/com/mathiasdarex/springdemo/mvc/Customer.java
@@ -1,9 +1,6 @@
 package com.mathiasdarex.springdemo.mvc;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -13,10 +10,13 @@ public class Customer {
     @Size(min=1, message = "is required")
     private String lastName;
 
-
     @Min(value = 0, message = "must be greater or equal to zero")
     @Max(value = 10, message = "must be less or equal to 10")
-    private int freePasses;
+    @NotNull(message = "is required")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -34,11 +34,19 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
