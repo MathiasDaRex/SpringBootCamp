56f3c548303fb4755d4737c335d2b20dd25dd9df
MathiasDaRex <87494005+MathiasDaRex@users.noreply.github.com>
2025-01-21 21:14:45 +0100

    Add files via upload
    now we can add new employees to the database by add employee form, made a repository method to sort the names by ascending

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/mathiasdarex/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/mathiasdarex/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,8 +3,17 @@ package com.mathiasdarex.springboot.thymeleafdemo.dao;
 import com.mathiasdarex.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code!
+
+    // add a method to sort by last name
+    // Spring Data JPA magic - makes the query by the name of the method behind the scenes
+    // here it's "FROM Employee ORDER BY lastName ASC"
+    // https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+
+    public List<Employee> findAllByOrderByLastNameAsc();
 
 }
