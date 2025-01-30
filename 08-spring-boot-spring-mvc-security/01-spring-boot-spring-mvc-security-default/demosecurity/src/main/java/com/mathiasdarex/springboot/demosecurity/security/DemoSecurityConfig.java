54f823c85622a0b21be0f4874573ef0dda617c7c
MathiasDaRex <87494005+MathiasDaRex@users.noreply.github.com>
2025-01-30 19:13:13 +0100

    Add files via upload
    made custom login page, configured the security config

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/demosecurity/src/main/java/com/mathiasdarex/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/demosecurity/src/main/java/com/mathiasdarex/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.mathiasdarex.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -31,6 +33,28 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john,mary,susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // Any request comes in it must be authenticated
+        // than login form url, processing url (no need for controller to it)
+        // and permission to see the login form
+
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                    form
+                            .loginPage("/showMyLoginPage")
+                            .loginProcessingUrl("/authenticateTheUser")
+                            .permitAll()
+                );
+
+        return http.build();
     }
 
 }
