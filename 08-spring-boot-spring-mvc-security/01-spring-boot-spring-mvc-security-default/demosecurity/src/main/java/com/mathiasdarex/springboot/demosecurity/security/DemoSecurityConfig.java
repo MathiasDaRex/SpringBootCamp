acfe1dcde721f5ecfcaab13e5989414c8e7384e7
MathiasDaRex <87494005+MathiasDaRex@users.noreply.github.com>
2025-02-11 20:26:10 +0100

    Add files via upload
    added database support, no more hardcoded users/passwords. now using MySQL with JDBC

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/demosecurity/src/main/java/com/mathiasdarex/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/demosecurity/src/main/java/com/mathiasdarex/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -3,37 +3,25 @@ package com.mathiasdarex.springboot.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // add support for JDBC ... no more hardcoded users :]
+    // dataSource is auto-configured by spring boot
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
 
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER","ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john,mary,susan);
+        // this tells spring security to use JDBC authentication with our data source
+        // that will know we use the pre-defined table schema, and looks for that - MAGIC -
+        return new JdbcUserDetailsManager(dataSource);
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -65,4 +53,28 @@ public class DemoSecurityConfig {
         return http.build();
     }
 
+/* @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER","ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john,mary,susan);
+    }
+*/
 }
